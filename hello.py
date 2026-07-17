373f1a7dbdbb7ebaf2526c2fa84ad29cd19c9147
Yassine Ghannay <yassineghannay@Yassines-MacBook-Air.local>
2026-07-16 20:39:19 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+print ("hello world")
+
+c = input("enter a character to check if its a vowel or not")
+
+if c == "i" or "e" or "o" or "u" or "a":
+    print ("its a vowel")
+else:
+    print ("its not a vowel son")
+
